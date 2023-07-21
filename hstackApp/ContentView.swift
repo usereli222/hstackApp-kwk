f5cc72c0063d5f5b66d83399c630f66a91d16b4e
emily <eli.mhs24@gmail.com>
2023-07-21 11:44:57 -0500

    Initial Commit

---
+++ hstackApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  hstackApp
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hstackApp/hstackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  hstackAppApp.swift
+//  hstackApp
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct hstackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
